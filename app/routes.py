5704841d3b71e316f7572efba5f22c13197440f7
Jeremy Bae <jswbae@gmail.com>
2018-04-02 23:50:55 +0900

    Fix XSS vulnerabilities to remove confusion

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import request, render_template_string, render_template
+from flask import request, render_template_string, render_template, Markup
 from app import app
 
 
@@ -7,31 +7,33 @@ def hello_world():
     user = {'username':"world", 'secret':"dG9wIHNlY3JldA=="}
     if request.args.get('username'):
         user['username'] = request.args.get('username')
-    template = '''<h2>Hi %s!</h2>''' % user['username']
+    template = Markup('''<h2>Hi %s!</h2>''') % user['username']
     return render_template_string(template, user=user)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    template = '''{%% extends "layout.html" %%}
+    template = Markup(
+'''{%% extends "layout.html" %%}
 {%% block content %%}
     <div class="center-content error">
         <h1>Oops! That page doesn't exist.</h1>
         <h3>%s</h3>
     </div>
 {%% endblock %%}
-''' % (request.url)
+''') % (request.url)
     return render_template_string(template), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    template = '''{%% extends "layout.html" %%}
+    template = Markup(
+'''{%% extends "layout.html" %%}
 {%% block content %%}
     <div class="center-content error">
         <h1>Oops! Something wrong!</h1>
         <h3>%s</h3>
     </div>
 {%% endblock %%}
-''' % (request.url)
+''') % (request.url)
     return render_template_string(template), 500
